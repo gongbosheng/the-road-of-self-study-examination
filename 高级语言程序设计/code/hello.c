eca355318bbb2d10f7ba2368aa54f974fa0c0a23
gongbosheng <gongbosheng@yeah.net>
2021-08-27 17:29:13 +0800

    高级语言程序设计笔记完结

--- 高级语言程序设计/code/hello.c
+++ 高级语言程序设计/code/hello.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void main()
 {   
-    int f = 8;
-    int *a = &f , *b = &f, *c = &f, d;
-    int *array[] = {a, b, c, &d};
-    printf("%d\n", *array[0]);
+    FILE *fp;
+    char ch[100];
+    char filename[10] =  "./1.txt";
+    fp = fopen(filename, "w+");
+    if (fp == NULL)
+    {
+        printf("文件打开错误!\n");
+        exit(0);
+    }
+
+    int seek = feof(fp);
+    printf("指针位置： %d\n", seek);
+
+    while (ch[0] != '#')
+    {
+        fprintf(fp,"%s\n",ch);
+        // fputs(ch,fp);
+        scanf("%s",ch);
+        // fgets(ch,20,stdin);
+        printf("%s", ch);
+        getchar();
+    }
+
+    rewind(fp);
+    char rn[1001];
+    while (1)
+    {
+        fgets(rn, 1001, fp);
+        if (feof(fp))
+        {
+            break;
+        }
+        printf("%s", rn);
+        
+    }
+    fclose(fp);
 }
