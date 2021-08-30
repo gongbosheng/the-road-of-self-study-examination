a78013dfa778c0f28b9d441f83f141447b0f278a
gongbosheng <gongbosheng@yeah.net>
2021-08-30 18:48:24 +0800

    开启数据结构的学习和笔记

---
+++ 数据结构导论/code/线性结构顺序存储/insert.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+const maxlength = 200;
+
+typedef struct
+{
+    int data[maxlength];
+    int length;
+} INSERT;
+
+void main()
+{
+    INSERT a;
+
+    // a.data = 
+}
---
+++ 高级语言程序设计/code/hanxin.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+void main()
+{
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("%d %d %d",a,b,c);
+}
